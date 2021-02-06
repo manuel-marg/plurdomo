bd5f2c869603e2fe052eb0f26bbbc43cf66ab27c
Manuel_Rodriguez <manuelrodriguez@correo.unimet.edu.ve>
2021-02-06 16:30:56 -0400

    Commit Inicial

---
+++ plurdomo-backend/sequelize/models/index.js
@@ -0,0 +1,17 @@
+//Conexion con la Base de Datos.
+import Sequelize from 'sequelize'
+
+
+const sequelize = new Sequelize('plurdomo', 'root', '12345678', { //Modifica los datos para conectarte a la Bd
+    host: '127.0.0.1',
+    dialect: 'mysql'
+})
+
+const models = {
+    persona: sequelize.import('./persona')
+}
+
+models.sequelize = sequelize
+models.Sequelize = Sequelize
+
+module.exports = models
